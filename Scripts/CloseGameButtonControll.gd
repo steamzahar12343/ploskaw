b249578dd795839fb58b201e92a825349f310e12
Mister <124846656+steamzahar12343@users.noreply.github.com>
2024-03-25 20:09:10 +0300

    Update
    Обновление кнопки

--- Scripts/CloseGameButtonControll.gd
+++ Scripts/CloseGameButtonControll.gd
@@ -1,7 +1,18 @@
 extends Button
 
-
+func _ready():
+	scale = Vector2(1.5,1.5)
+	position = Vector2(-7, 0)
 
 
 func _on_pressed():
 	get_tree().quit()
+
+func _on_mouse_entered():
+	scale = Vector2(7,7)
+	position = Vector2(-63, 0)
+
+
+func _on_mouse_exited():
+	scale = Vector2(1.5,1.5)
+	position = Vector2(-7, 0)
